35c2382dfb3f2b094768587eb1b913f2fad63427
Vkxm <Vikashkumar244@outlook.com>
2025-06-01 19:28:13 +0530

    Add project files.

---
+++ first_test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    public static void Main()
+    {
+        
+            string input = " t ";
+            string pattern = @"\s{1}";
+        
+
+            bool isMobile = Regex.IsMatch(input, pattern);
+
+            Console.WriteLine(isMobile);  
+        
+    }
+}
